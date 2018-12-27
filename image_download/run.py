003bf6efea1a7a727da46174a635ae08b1cf6d28
xinzhu_ye <xinzhu_ye@albert2005.co.jp>
2018-12-27 15:27:34 +0900

    update:
    1. add download_delay(=3 seconds)
    2. use multi-thread instead of map_function

--- image_download/run.py
+++ image_download/run.py
@@ -1,6 +1,8 @@
+from multiprocessing.dummy import Pool as ThreadPool
 import os
 import pandas as pd
 import requests
+from time import sleep
 
 
 def image_download(info):
@@ -13,6 +15,7 @@ def image_download(info):
     image_file = 'image/' + info[0].replace('/', '_') + '.jpg'
     image_url = info[1]
 
+    sleep(3)
     r = requests.get(image_url)
     with open(image_file, 'wb') as f:
         f.write(r.content)
@@ -26,9 +29,27 @@ def start_crawling(image_data_file, download_folder='image/'):
     # image folder
     os.makedirs(download_folder, exist_ok=True)
 
+    pool = ThreadPool()
+
     df = pd.read_csv(image_data_file)
 
-    result = list(map(image_download, list(zip(df['id'], df['image_url']))))
+    '''
+    if download 30 times, it'll spend:
+        map function：94.9s
+        Pool：25.2s
+        ThreadPool：26.5s
+
+    if download 120 times, it'll spend:
+        Pool：103.9s
+        ThreadPool：99.6s
+
+    so use ThreadPool
+    '''
+    results = pool.map(image_download, list(zip(df['id'], df['image_url'])))
+
+    # close the pool and wait for the work to finish
+    pool.close()
+    pool.join()
 
 
 start_crawling(image_data_file="data/image_data_test.csv")
--- tabelog_spider/tabelog_spider/spiders/tabelog_review.py
+++ tabelog_spider/tabelog_spider/spiders/tabelog_review.py
@@ -1,11 +1,7 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import scrapy
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
-from scrapy.utils.project import get_project_settings
 import pandas as pd
-from twisted.internet import reactor
 
 
 class TabelogSpider(scrapy.Spider):
